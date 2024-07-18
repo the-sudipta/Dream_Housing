2b12031f5e4c681b38809a9475cd18fa4021620c
Sudipta <engineer.sudipta.kumar.das@gmail.com>
2024-07-18 14:20:12 +0600

    Shifting to the fast track project

--- Dream_Housing/Database/DatabaseContext.cs
+++ Dream_Housing/Database/DatabaseContext.cs
@@ -9,13 +9,7 @@ namespace Dream_Housing.Database
 
 	public class DatabaseContext : DbContext
 	{
-		//public DatabaseContext() : base("name=DatabaseContext")
-		//{
-		//}
-
-
 		public DbSet<User> Users { get; set; }
-
 
 	}
 
